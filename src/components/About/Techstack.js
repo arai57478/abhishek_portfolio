87772f2b0359cd36d240ae0a9be19e9766a91083
abhishek <arai57478@gmail.com>
2022-03-16 23:31:29 +0530

    change

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
+
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
   DiJava,
-  DiGit,
+ 
 } from "react-icons/di";
 import {FaAws} from "react-icons/fa";
-import { SiPytorch, SiTensorflow, SiFirebase } from "react-icons/si";
+import {  SiTensorflow, SiFirebase } from "react-icons/si";
 
 function Techstack() {
   return (
